4cf17d865a7c4f2247f4b8d658521f445b971763
Dankrm <danton.mohr@gmail.com>
2021-07-09 21:13:52 -0300

    login

--- apiagenda/usuario/urls.py
+++ apiagenda/usuario/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 from apiagenda.usuario.views import UsuarioViewSet
+from .views import validate_login
 
 api_router = routers.DefaultRouter()
 api_router.register(r"usuario", UsuarioViewSet)
 
 urlpatterns = [
     path("api/", include(api_router.urls)),
+    path("login/", validate_login)
 ]
